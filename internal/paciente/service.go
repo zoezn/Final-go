2ad615699f303e9130b2705e83e27209348fb12d
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2023-07-02 17:14:10 -0300

    fixeado el problema del dni resetandose

--- internal/paciente/service.go
+++ internal/paciente/service.go
@@ -1,8 +1,6 @@
 package paciente
 
 import (
-	"strconv"
-
 	"github.com/zoezn/Final-go/internal/domain"
 )
 
@@ -54,7 +52,7 @@ func (s *service) Update(id int, u domain.Paciente) (domain.Paciente, error) {
 		p.Domicilio = u.Domicilio
 	}
 
-	if strconv.Itoa(u.DNI) != "" {
+	if u.DNI != 0 {
 		p.DNI = u.DNI
 	}
 
